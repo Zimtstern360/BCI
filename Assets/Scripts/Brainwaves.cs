b442d4b6dec764c9fa3d16daf432c3e2f99a6e78
Steffi <braun.stephanie@hotmail.de>
2015-01-22 12:44:55 +0100

    starting new

--- Assets/Scripts/Brainwaves.cs
+++ Assets/Scripts/Brainwaves.cs
@@ -5,8 +5,12 @@ using System.Collections.Generic;
 public class Brainwaves : MonoBehaviour
 {
     private List<GameObject> _pointsList = new List<GameObject>();
+    private List<GameObject> _fillerPointsList = new List<GameObject>();
     private List<float> _xCoordList = new List<float>();
     public GameObject _singlePoint;
+
+    public GameObject _parentPoints;
+    public GameObject _parentFillerPoints;
 
     public Camera _MainCamera;
     public int _Screenwidth = 1024;
@@ -28,6 +32,8 @@ public class Brainwaves : MonoBehaviour
     
     void Update()
     {
+        RandomCoords();
+
         if(_xCoordList.Count > 20)
         {
             _xCoordList.RemoveAt(0);
@@ -35,7 +41,12 @@ public class Brainwaves : MonoBehaviour
             {
                 DestroyImmediate(_singlePoint);
             }
+            foreach (GameObject _fillerPoint in _fillerPointsList)
+            {
+                DestroyImmediate(_fillerPoint);
+            }
             _pointsList.Clear();
+            _fillerPointsList.Clear();
             BuildPoints();
         }
     }
@@ -51,10 +62,21 @@ public class Brainwaves : MonoBehaviour
     {
         for (int i = 0; i < 20; i++)
         {
-            GameObject _point = Instantiate(_singlePoint, new Vector3(i * 0.25F, _xCoordList[i], 0), Quaternion.identity) as GameObject;
-            _point.transform.parent = transform;
+            GameObject _point = Instantiate(_singlePoint, new Vector3(i * 0.125F, _xCoordList[i], 0), Quaternion.identity) as GameObject;
+            Debug.Log("Single Point: "+_singlePoint.transform.position.y);
+            _point.transform.parent = _parentPoints.transform;
             _pointsList.Add(_point);
+
+            GameObject _fillerPoint = Instantiate(_singlePoint, new Vector3(i * 0.125f, ((_point.transform.position.y > this.transform.position.y) ? (_point.transform.position.y * 0.9f) : 1), (_point.transform.position.y * 1.1f)), Quaternion.identity) as GameObject;
+            Debug.Log("Filler Point: " + _fillerPoint.transform.position.y);
+            _fillerPoint.transform.parent = _parentFillerPoints.transform;
+            _fillerPointsList.Add(_fillerPoint);
         }
+    }
+
+    void BuildFillerPoints()
+    {
+        
     }
 
     void MaxCoord()
@@ -82,5 +104,12 @@ public class Brainwaves : MonoBehaviour
     {
         Screen.SetResolution(_Screenwidth, _Screenheight, false);
     }
+
+
+    void RandomCoords()
+    {
+        _xCoordList.Add(Random.Range(-1.0f, 1.0f));
+    }
+
 
 }
---
+++ Assets/Scripts/Interface.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Interface : MonoBehaviour {
+
+    private Text _max;
+    private Text _min;
+    private Text _avg;
+
+    private float _maximum = 0.0f;
+    private float _minimum = 0.0f;
+    private float _average = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+
+        _max = gameObject.GetComponent<Text>();
+        _min = gameObject.GetComponent<Text>();
+        _avg = gameObject.GetComponent<Text>();
+
+        if(gameObject.tag == "Maximum")
+        {
+            _max.text = "Maximum: ";
+        }
+        if(gameObject.tag == "Minimum")
+        {
+            _min.text = "Minimum: ";
+        }
+        if (gameObject.tag == "Average")
+        {
+            _avg.text = "Average: ";
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (gameObject.tag == "Maximum")
+        {
+            _max.text = "Maximum: " + _maximum;
+        }
+        if (gameObject.tag == "Minimum")
+        {
+            _min.text = "Minimum: " + _minimum;
+        }
+        if (gameObject.tag == "Average")
+        {
+            _avg.text = "Average: " + _average;
+        }
+
+	}
+}
+
+
+
+/*
+     // Update is called once per frame
+     void Update () {
+         txt.text="Score : " + currentscore;  
+         currentscore = PlayerPrefs.GetInt("TOTALSCORE"); 
+         PlayerPrefs.SetInt("SHOWSTARTSCORE",currentscore); 
+     }*/
